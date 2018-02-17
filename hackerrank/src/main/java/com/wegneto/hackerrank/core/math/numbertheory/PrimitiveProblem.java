46a73a7d9026225a8ea5aea9077f514eae0f9736
Wilson Guimarães <wegneto@gmail.com>
2018-02-17 18:41:25 +0000

    [wip] Primitive problem

---
+++ hackerrank/src/main/java/com/wegneto/hackerrank/core/math/numbertheory/PrimitiveProblem.java
@@ -0,0 +1,11 @@
+package com.wegneto.hackerrank.core.math.numbertheory;
+
+public class PrimitiveProblem {
+
+	public static void main(String[] args) {
+		for(int i = 0; i <= 5; i++) {
+			System.out.println(Math.pow(3, i) % 7);			
+		}
+	}
+	
+}
