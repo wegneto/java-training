34a30eceb07701598874adb94b6f4d2184a8e1bf
Wilson Guimarães <wegneto@gmail.com>
2018-02-18 21:28:29 +0000

    Java Stdin and Stdout II

---
+++ hackerrank/src/main/java/com/wegneto/hackerrank/java/intro/StdinAndStdout2.java
@@ -0,0 +1,20 @@
+package com.wegneto.hackerrank.java.intro;
+
+import java.util.Scanner;
+
+public class StdinAndStdout2 {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+        int i = scan.nextInt();
+        // Write your code here.
+        double d = scan.nextDouble();
+        scan.nextLine();
+        String s = scan.nextLine();
+
+        System.out.println("String: " + s);
+        System.out.println("Double: " + d);
+        System.out.println("Int: " + i);
+    }
+	
+}
