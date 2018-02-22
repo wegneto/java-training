8ec99d8830a5c4cbe1e816ed531ff9313f435a69
Wilson Guimarães <wegneto@gmail.com>
2018-02-22 21:25:52 +0000

    Java Loops I

--- hackerrank/src/main/java/com/wegneto/hackerrank/java/intro/JavaLoops.java
+++ hackerrank/src/main/java/com/wegneto/hackerrank/java/intro/JavaLoops.java
@@ -9,7 +9,7 @@ public class JavaLoops {
         int n = scan.nextInt();
         
         for (int i = 1; i < 11; i++) {
-        		System.out.printf("%d X %d = %d\n", n, i, (n*i));
+        		System.out.printf("%d x %d = %d\n", n, i, (n*i));
         }
 	}
 
