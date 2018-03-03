2a482da384c9d1377c331a2141db03e0d01a49f1
Wilson Guimarães <wegneto@gmail.com>
2018-03-03 10:22:15 +0000

    Java Date and Time challenge

---
+++ hackerrank/src/main/java/com/wegneto/hackerrank/java/intro/DateAndTime.java
@@ -0,0 +1,29 @@
+package com.wegneto.hackerrank.java.intro;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Scanner;
+
+public class DateAndTime {
+
+	public static String getDay(String day, String month, String year) {
+		Calendar calendar = Calendar.getInstance();
+		calendar.set(Integer.valueOf(year), Integer.valueOf(month)-1, Integer.valueOf(day));
+
+		SimpleDateFormat dateFormat = new SimpleDateFormat("EEEEE");
+		String dayOfWeek = dateFormat.format(calendar.getTime());
+		return dayOfWeek.toUpperCase();
+
+	}
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		String month = in.next();
+		String day = in.next();
+		String year = in.next();
+		in.close();
+
+		System.out.println(getDay(day, month, year));
+	}
+
+}
