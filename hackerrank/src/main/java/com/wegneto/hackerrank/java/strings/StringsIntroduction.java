50ff4f38ed520e02df1d6cdb6949d5c256a592ab
Wilson Guimarães <wegneto@gmail.com>
2018-03-06 10:43:39 +0000

    Java Strings Introduction challenge

--- hackerrank/src/main/java/com/wegneto/hackerrank/java/strings/StringsIntroduction.java
+++ hackerrank/src/main/java/com/wegneto/hackerrank/java/strings/StringsIntroduction.java
@@ -1,5 +1,21 @@
 package com.wegneto.hackerrank.java.strings;
 
+import java.util.Scanner;
+
 public class StringsIntroduction {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String A = sc.next();
+		String B = sc.next();
+		/* Enter your code here. Print output to STDOUT. */
+		sc.close();
+
+		System.out.println(A.length() + B.length());
+		System.out.println(A.compareTo(B) > 0 ? "Yes" : "No");
+		A = A.substring(0, 1).toUpperCase() + A.substring(1);
+		B = B.substring(0, 1).toUpperCase() + B.substring(1);
+		System.out.println(A + " " + B);
+	}
 
 }
