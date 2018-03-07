6715de0cb0893fd99c9678036f0f77e67f334073
Wilson Guimarães <wegneto@gmail.com>
2018-03-07 09:01:25 +0000

    Adding new challenges

---
+++ hackerrank/src/main/java/com/wegneto/hackerrank/challenges/MaxPotentialProfit.java
@@ -0,0 +1,15 @@
+package com.wegneto.hackerrank.challenges;
+
+/*
+You are given a series of stock prices for some unspecified time period. At each point in time you could either buy one piece of stock at the given price, or sell the stock you have bought at that price. Your goal is to write a method that calculates the maximum possible profit you could have if you buy once and sell once.
+
+Example:
+input: 5, 3, 9, 2, 7, 10, 1, 6
+-> maximum potential profit 8: (10 - 2)
+
+You can observe the whole sequence.
+
+*/
+public class MaxPotentialProfit {
+
+}
---
+++ hackerrank/src/main/java/com/wegneto/hackerrank/challenges/ReverseString.java
@@ -0,0 +1,12 @@
+package com.wegneto.hackerrank.challenges;
+
+/*
+Write a function that reverses the word order in a given string.
+
+e.g.
+I am in the correct order
+-> order correct the in am I
+*/
+public class ReverseString {
+
+}
