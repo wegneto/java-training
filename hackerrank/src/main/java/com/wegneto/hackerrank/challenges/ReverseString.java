c6c55011d4d1bcba4350de5166faf3c1384b9cca
Wilson Guimarães <wegneto@gmail.com>
2018-03-12 22:08:41 +0000

    Write a string in the reverse order.

--- hackerrank/src/main/java/com/wegneto/hackerrank/challenges/ReverseString.java
+++ hackerrank/src/main/java/com/wegneto/hackerrank/challenges/ReverseString.java
@@ -9,4 +9,18 @@ I am in the correct order
 */
 public class ReverseString {
 
+	public static void reverse(String str) {
+		String[] reverse = str.split(" ");
+		for (int i = reverse.length - 1; i >= 0; i--) {
+			System.out.print(reverse[i]);
+			if (i != 0) {
+				System.out.print(" ");
+			}
+		}
+	}
+	
+	public static void main(String[] args) {
+		reverse("I am in the correct order");
+	}
+	
 }
