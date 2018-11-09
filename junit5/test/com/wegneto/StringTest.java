8ca89edfc4f81333d8dc5b2b5caae4c0455e88fd
Wilson Guimarães <wegneto@gmail.com>
2018-11-09 23:23:08 +0000

    Step 5.

--- junit5/test/com/wegneto/StringTest.java
+++ junit5/test/com/wegneto/StringTest.java
@@ -1,16 +1,33 @@
 package com.wegneto;
 
+import static org.junit.Assert.assertEquals;
+
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 class StringTest {
 
 	@Test
-	void test() {
+	void length() {
 		int actualLength = "ABCD".length();
 		int expectedLength = 4;
 		
 		Assertions.assertEquals(expectedLength, actualLength);
+	}
+	
+	@Test
+	void toUpperCase() {
+		String str = "abcd";
+		String result = str.toUpperCase();
+		assertEquals("ABCD", result);
+	}
+	
+	@Test
+	void contains() {
+		String str = "abcdefgh";
+		boolean result = str.contains("ijk");
+		
+		assertEquals(false, result);
 	}
 
 }
