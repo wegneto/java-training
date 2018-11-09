c9637420cafc83382c3004a96de13ce0bd0c158c
Wilson Guimarães <wegneto@gmail.com>
2018-11-09 23:05:54 +0000

    Step 02. First JUnit test - Red bar

---
+++ junit5/test/com/wegneto/StringTest.java
@@ -0,0 +1,14 @@
+package com.wegneto;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class StringTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
