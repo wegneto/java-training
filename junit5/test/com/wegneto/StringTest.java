5a975cb4f4eaaa260f0f00a7560bdb0d93d0009d
Wilson Guimarães <wegneto@gmail.com>
2018-11-09 23:13:30 +0000

    Step 4.

--- junit5/test/com/wegneto/StringTest.java
+++ junit5/test/com/wegneto/StringTest.java
@@ -1,14 +1,16 @@
 package com.wegneto;
 
-import static org.junit.jupiter.api.Assertions.*;
-
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 class StringTest {
 
 	@Test
 	void test() {
-		int length = "ABCD".length();
+		int actualLength = "ABCD".length();
+		int expectedLength = 4;
+		
+		Assertions.assertEquals(expectedLength, actualLength);
 	}
 
 }
