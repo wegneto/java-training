05073f961debe6983f1c90a4eac8e1be99b1db22
Wilson Guimarães <wegneto@gmail.com>
2018-11-12 18:49:48 +0000

    Step 9

--- junit5/test/com/wegneto/StringTest.java
+++ junit5/test/com/wegneto/StringTest.java
@@ -10,22 +10,22 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInfo;
 
 class StringTest {
-	
+
 	@BeforeAll
 	static void beforeAll() {
 		System.out.println("Doing some generic initialization..");
 	}
-	
+
 	@AfterAll
 	static void afterAll() {
 		System.out.println("Doing some generic clean up..");
 	}
-	
+
 	@BeforeEach
 	void beforeEach(TestInfo info) {
 		System.out.println("Initialize Test Data for: " + info.getDisplayName());
 	}
-	
+
 	@AfterEach
 	void afterEach(TestInfo info) {
 		System.out.println("Clean up Test Data for: " + info.getDisplayName());
@@ -35,29 +35,37 @@ class StringTest {
 	void length() {
 		int actualLength = "ABCD".length();
 		int expectedLength = 4;
-		
+
 		assertEquals(expectedLength, actualLength);
 	}
-	
+
 	@Test
 	void toUpperCase() {
 		String str = "abcd";
 		String result = str.toUpperCase();
 		assertEquals("ABCD", result);
 	}
-	
+
 	@Test
 	void contains() {
 		assertFalse("abcdefgh".contains("ijk"));
 	}
-	
+
 	@Test
 	void split() {
 		String str = "abc def ghi";
 		String[] actualResult = str.split(" ");
-		String[] expectedResult = new String[] {"abc", "def", "ghi"};
-		
+		String[] expectedResult = new String[] { "abc", "def", "ghi" };
+
 		assertArrayEquals(expectedResult, actualResult);
+	}
+
+	@Test
+	void lengthException() {
+		String str = null;
+		assertThrows(NullPointerException.class, () -> {
+			str.length();
+		});
 	}
 
 }
