9ab1b84e383df8abea342e31c29429813172d392
Wilson Guimarães <wegneto@gmail.com>
2018-11-12 18:39:34 +0000

    Step 7

--- junit5/test/com/wegneto/StringTest.java
+++ junit5/test/com/wegneto/StringTest.java
@@ -2,9 +2,22 @@ package com.wegneto;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInfo;
 
 class StringTest {
+	
+	@BeforeEach
+	void beforeEach(TestInfo info) {
+		System.out.println("Initialize Test Data for: " + info.getDisplayName());
+	}
+	
+	@AfterEach
+	void afterEach(TestInfo info) {
+		System.out.println("Clean up Test Data for: " + info.getDisplayName());
+	}
 
 	@Test
 	void length() {
