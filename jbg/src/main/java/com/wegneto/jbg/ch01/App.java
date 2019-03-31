4823ec70fce4abaa29f4203eac3e9db77906a50d
Wilson Guimarães <wegneto@gmail.com>
2019-03-31 21:39:50 +0100

    Creating project

---
+++ jbg/src/main/java/com/wegneto/jbg/ch01/App.java
@@ -0,0 +1,13 @@
+package com.wegneto.jbg.ch01;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
