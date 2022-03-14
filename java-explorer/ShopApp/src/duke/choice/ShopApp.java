00fe5b24130c0bb3e1c0081de1551d3b5aaf49a0
Wilson Guimarães <wilson.guimaraes@trustly.com>
2022-03-14 13:51:37 +0000

    Exercise 3-1

--- java-explorer/ShopApp/src/duke/choice/ShopApp.java
+++ java-explorer/ShopApp/src/duke/choice/ShopApp.java
@@ -5,5 +5,9 @@ public class ShopApp {
     public static void main(String[] args) {
         // write your code here
         System.out.println("Welcome to Duke Choice Shop");
+
+        Customer c1 = new Customer();
+        c1.name = "Pinky";
+        System.out.println("Hello " + c1.name + ".");
     }
 }
