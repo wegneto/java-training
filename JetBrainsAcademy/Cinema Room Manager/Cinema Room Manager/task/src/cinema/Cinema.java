9b279917fb0c778584a5df3c337c114b0a3cc418
Wilson Guimarães <wegneto@gmail.com>
2022-03-26 09:39:47 +0000

    Cinema Challenge 2/5

--- JetBrainsAcademy/Cinema Room Manager/Cinema Room Manager/task/src/cinema/Cinema.java
+++ JetBrainsAcademy/Cinema Room Manager/Cinema Room Manager/task/src/cinema/Cinema.java
@@ -1,12 +1,23 @@
 package cinema;
 
+import java.util.Scanner;
+
 public class Cinema {
 
     public static void main(String[] args) {
-        Room room = new Room();
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the number of rows:");
+        int rows = Integer.parseInt(scanner.next());
 
-        System.out.println("Cinema:");
-        System.out.println(room);
+        System.out.println("Enter the number of seats in each row:");
+        int seats = Integer.parseInt(scanner.next());
+
+        Room room = new Room(rows, seats);
+        int income = room.calculateIncome();
+        System.out.printf("Total income:\n$%d", income);
+
+        //System.out.println("Cinema:");
+        //System.out.println(room);
     }
 }
 
--- JetBrainsAcademy/Cinema Room Manager/Cinema Room Manager/task/src/cinema/Room.java
+++ JetBrainsAcademy/Cinema Room Manager/Cinema Room Manager/task/src/cinema/Room.java
@@ -2,42 +2,48 @@ package cinema;
 
 public class Room {
 
-    private final int ROWS = 7;
-    private final int SEATS_PER_ROW = 8;
+    private int rows;
+    private int seatsPerRow;
 
-    private Seat[][] seats;
+    private Seat[][] seatMap;
 
-    public Room() {
-        seats = new Seat[ROWS][SEATS_PER_ROW];
+    public Room(int rows, int seatsPerRow) {
+        this.rows = rows;
+        this.seatsPerRow = seatsPerRow;
 
-        for (int i = 0; i < seats.length; i++) {
-            for (int j = 0; j < seats[i].length; j++) {
-                seats[i][j] = new Seat();
+        seatMap = new Seat[rows][seatsPerRow];
+
+        for (int i = 0; i < seatMap.length; i++) {
+            for (int j = 0; j < seatMap[i].length; j++) {
+                seatMap[i][j] = new Seat();
             }
         }
     }
 
-    public Seat[][] getSeats() {
-        return seats;
+    public int calculateIncome() {
+        int income = rows * seatsPerRow * 10;
+
+        if (income > 600) {
+            income -= ((rows+1)/2) * seatsPerRow * 2;
+        }
+
+        return income;
     }
 
-    public void setSeats(Seat[][] seats) {
-        this.seats = seats;
-    }
 
     @Override
     public String toString() {
         StringBuffer value = new StringBuffer();
-        for (int i = 0; i <= seats[0].length; i++) {
+        for (int i = 0; i <= seatMap[0].length; i++) {
             value.append((i == 0 ? " " : i) + " ");
         }
 
         value.append("\n");
 
-        for (int i = 0; i < seats.length; i++) {
+        for (int i = 0; i < seatMap.length; i++) {
             value.append(i + 1 + " ");
-            for (int j = 0; j < seats[i].length; j++) {
-                value.append(seats[i][j] + " ");
+            for (int j = 0; j < seatMap[i].length; j++) {
+                value.append(seatMap[i][j] + " ");
             }
 
             value.append("\n");
@@ -46,4 +52,6 @@ public class Room {
 
         return value.toString();
     }
+
+
 }
