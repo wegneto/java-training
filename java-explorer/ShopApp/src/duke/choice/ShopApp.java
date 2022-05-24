4e0f416628dba4d6ce4503449f27de81fa4ccce8
Wilson Guimarães <wegneto@gmail.com>
2022-05-24 11:05:24 +0100

    Cloud deployment demo

--- java-explorer/ShopApp/src/duke/choice/ShopApp.java
+++ java-explorer/ShopApp/src/duke/choice/ShopApp.java
@@ -20,7 +20,7 @@ public class ShopApp {
         System.out.println("The minimum price is: " + Clothing.MIN_PRICE);
 
         Clothing item1 = new Clothing("Blue Jacket", 20.9, "M");
-        Clothing item2 = new Clothing("Orange T-Shirt", 10.5,"S");
+        Clothing item2 = new Clothing("Orange T-Shirt", 10.5, "S");
         Clothing item3 = new Clothing("Green Scarf", 5, "S");
         Clothing item4 = new Clothing("Blue T-Shirt", 10.5, "S");
 
